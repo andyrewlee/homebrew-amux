1018ce97d8664346493d308902f0acb5b0f0ffa1
Andrew Lee <andyrewlee@gmail.com>
2026-02-02 17:39:29 -0800

    Add amux formula

---
+++ Formula/amux.rb
@@ -0,0 +1,24 @@
+class Amux < Formula
+  desc "TUI for running parallel coding agents"
+  homepage "https://github.com/andyrewlee/amux"
+  url "https://github.com/andyrewlee/amux/archive/refs/tags/v0.0.10.tar.gz"
+  sha256 "c05cbe3cd6fbec1cadd1c675a75dc065a305117eb94ece4cc29afd8dd79c939e"
+  license "MIT"
+
+  depends_on "go" => :build
+  depends_on "tmux"
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X github.com/andyrewlee/amux/internal/update.homebrewBuild=true
+    ]
+    system "go", "build", *std_go_args(ldflags: ldflags), "./cmd/amux"
+  end
+
+  test do
+    output = shell_output("#{bin}/amux --version")
+    assert_match version.to_s, output
+  end
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Homebrew Tap for amux
+
+This tap provides the Homebrew formula for amux.
+
+## Install
+
+```bash
+brew tap andyrewlee/amux
+brew install amux
+```
+
+## Upgrade
+
+```bash
+brew upgrade amux
+```
